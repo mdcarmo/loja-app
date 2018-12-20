e7cc75a15f38caf07750288dc945d74125751fc7
Dias do Carmo <marcelo.carmo@scania.com>
2018-12-20 17:56:39 -0200

    alteracao layout

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  totalproduct: string;
+  totalsoldProduct: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.totalproduct = "500";
+    this.totalsoldProduct = "R$ 5.000,00";
   }
-
 }
